fa4b2cc643e07b4b0f60f3bfa7408912c2e3a4d7
Kneelawk <jedidiah.pommert@gmail.com>
2018-01-12 00:28:26 -0800

    ModelRenderers should be built initialized

--- src/main/scala/org/kneelawk/learningopengl/ModelRenderer.scala
+++ src/main/scala/org/kneelawk/learningopengl/ModelRenderer.scala
@@ -2,17 +2,12 @@ package org.kneelawk.learningopengl
 
 trait ModelRenderer[Model] {
   /**
-   * Setup OpenGL stuff (vertex arrays, etc.)
-   */
-  def init()
-  
-  /**
-   * Is this renderer initialized?
-   */
-  def initialized: Boolean
-  
-  /**
    * Called to render a batch of models.
    */
   def render(models: Iterable[AnyRef], camera: Camera)
+  
+  /**
+   * Release OpenGL stuff (vertex arrays, etc.)
+   */
+  def destroy()
 }
