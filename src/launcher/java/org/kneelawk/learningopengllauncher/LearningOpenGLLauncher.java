77706b337ae478880716b43412d55199cce64ba7
Kneelawk <jedidiah.pommert@gmail.com>
2018-09-15 21:35:40 -0700

    Move to kwrapper-gradle 0.0.5 and CPControl 4.0.0

--- src/launcher/java/org/kneelawk/learningopengllauncher/LearningOpenGLLauncher.java
+++ src/launcher/java/org/kneelawk/learningopengllauncher/LearningOpenGLLauncher.java
@@ -2,29 +2,29 @@ package org.kneelawk.learningopengllauncher;
 
 import java.io.IOException;
 
-import org.kneelawk.cpcontrol.CPControl3;
-import org.kneelawk.cpcontrol.CPControl3.AndEntryFilter;
-import org.kneelawk.cpcontrol.CPControl3.DirectoryEntryFilter;
-import org.kneelawk.cpcontrol.CPControl3.NameContainsEntryFilter;
+import com.github.kneelawk.cpcontrol.AndEntryFilter;
+import com.github.kneelawk.cpcontrol.CPControl4;
+import com.github.kneelawk.cpcontrol.DirectoryEntryFilter;
+import com.github.kneelawk.cpcontrol.NameContainsEntryFilter;
 
 public class LearningOpenGLLauncher {
 
 	public static void main(String[] args) {
 		// use org.kneelawk.klines.KLines because scala main objects create
 		// companion classes
-		CPControl3 c = new CPControl3("org.kneelawk.learningopengl.LearningOpenGL");
+		CPControl4 c = new CPControl4("org.kneelawk.learningopengl.LearningOpenGL");
 
 		// Use extractingFromFile version because extractingFromClasspath
 		// version searches entire classpath
-		c.addExtractingFromFileLibrary(CPControl3.ME)
+		c.addExtractingFromFileLibrary(CPControl4.ME)
 				// Don't forget to add the application jar
-				.addLibrary("application", new DirectoryEntryFilter("app"), CPControl3.ALWAYS_DELETE)
+				.addLibrary("application", new DirectoryEntryFilter("app"), CPControl4.ALWAYS_DELETE)
 				.addLibrary("scala-lang",
 						new AndEntryFilter(new DirectoryEntryFilter("libs"), new NameContainsEntryFilter("scala")),
-						CPControl3.ALWAYS_DELETE)
+						CPControl4.ALWAYS_DELETE)
 				.addLibrary("lwjgl",
 						new AndEntryFilter(new DirectoryEntryFilter("libs"), new NameContainsEntryFilter("lwjgl")),
-						CPControl3.ALWAYS_DELETE);
+						CPControl4.ALWAYS_DELETE);
 
 		try {
 			c.launch(args);
