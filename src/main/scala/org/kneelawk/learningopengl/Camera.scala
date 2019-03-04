9d176de1f29dc64d6b7da3d512a72a447e44c550
Kneelawk <jedidiah.pommert@gmail.com>
2019-03-03 17:54:15 -0800

    Add more parts to the Camera

--- src/main/scala/org/kneelawk/learningopengl/Camera.scala
+++ src/main/scala/org/kneelawk/learningopengl/Camera.scala
@@ -3,8 +3,16 @@ package org.kneelawk.learningopengl
 import org.joml.{Matrix4f, Vector3f}
 
 class Camera {
-  private val view = new Matrix4f()
-  private val projection = new Matrix4f()
+  private val location = new Vector3f(0, 0, 1)
+  private val target = new Vector3f(0, 0, 0)
+  private val up = new Vector3f(0, 1, 0)
+  private var fovy = 45f
+  private var aspect = 1f
+  private var zNear = 0.01f
+  private var zFar = 100f
+
+  private val view = new Matrix4f().setLookAt(location, target, up)
+  private val projection = new Matrix4f().setPerspective(fovy, aspect, zNear, zFar)
 
   private val pv = projection.mul(view, new Matrix4f())
 
@@ -13,25 +21,95 @@ class Camera {
    */
   def getMatrix: Matrix4f = pv
 
+  def getLocation: Vector3f = location
+  def getTarget: Vector3f = target
+  def getUp: Vector3f = up
+
+  def getFovy: Float = fovy
+  def getAspect: Float = aspect
+  def getZNear: Float = zNear
+  def getZFar: Float = zFar
+
+  def getView: Matrix4f = view
+  def getProjection: Matrix4f = projection
+
   /**
-   * Recalculates the camera's view-projection matrix.
+   * Recalculates the camera's matrices.
    */
   def update() {
+    view.setLookAt(location, target, up)
+    projection.setPerspective(fovy, aspect, zNear, zFar)
     projection.mul(view, pv)
+  }
+
+  def setLocation(location: Vector3f): Unit = {
+    this.location.set(location)
+  }
+
+  def setLocation(locationX: Float, locationY: Float, locationZ: Float): Unit = {
+    location.x = locationX
+    location.y = locationY
+    location.z = locationZ
+  }
+
+  def setTarget(target: Vector3f): Unit = {
+    this.target.set(target)
+  }
+
+  def setTarget(targetX: Float, targetY: Float, targetZ: Float): Unit = {
+    target.x = targetX
+    target.y = targetY
+    target.z = targetZ
+  }
+
+  def setUp(up: Vector3f): Unit = {
+    this.up.set(up)
+  }
+
+  def setUp(upX: Float, upY: Float, upZ: Float): Unit = {
+    up.x = upX
+    up.y = upY
+    up.z = upZ
+  }
+
+  def setFovy(fovy: Float): Unit = {
+    this.fovy = fovy
+  }
+
+  def setAspect(aspect: Float): Unit = {
+    this.aspect = aspect
+  }
+
+  def setZNear(zNear: Float): Unit = {
+    this.zNear = zNear
+  }
+
+  def setZFar(zFar: Float): Unit = {
+    this.zFar = zFar
   }
 
   /**
    * Sets the camera's location, target sight, and up direction.
    */
   def setView(location: Vector3f, target: Vector3f, up: Vector3f) {
-    view.setLookAt(location, target, up)
+    this.location.set(location)
+    this.target.set(target)
+    this.up.set(up)
   }
 
   /**
    * Sets the camera's location, target sight, and up direction.
    */
   def setView(locationX: Float, locationY: Float, locationZ: Float, targetX: Float, targetY: Float, targetZ: Float, upX: Float, upY: Float, upZ: Float) {
-    view.setLookAt(locationX, locationY, locationZ, targetX, targetY, targetZ, upX, upY, upZ)
+    location.x = locationX
+    location.y = locationY
+    location.z = locationZ
+    target.x = targetX
+    target.y = targetY
+    target.z = targetZ
+    up.x = upX
+    up.y = upY
+    up.z = upZ
   }
 
   /**
@@ -39,6 +117,9 @@ class Camera {
    */
   //noinspection SpellCheckingInspection,SpellCheckingInspection
   def setProjection(fovy: Float, aspect: Float, zNear: Float, zFar: Float) {
-    projection.setPerspective(fovy, aspect, zNear, zFar)
+    this.fovy = fovy
+    this.aspect = aspect
+    this.zNear = zNear
+    this.zFar = zFar
   }
 }
